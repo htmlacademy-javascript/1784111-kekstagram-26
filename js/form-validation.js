c700376d0750593ada383f8ec1e2094c52940731
SergeyY1994 <serrrrrrrrr@yandex.ru>
2022-07-03 15:16:58 +0300

    Добавляет валидацию

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,48 +1,51 @@
-const form = document.querySelector('.img-upload__form');
+import {userFormElement} from './user-form.js';
 
-const pristine = new Pristine(form, {
+const pristine = new Pristine(userFormElement, {
   classTo: 'text__label',
   errorTextParent: 'text__label',
 });
 
 const re = /^#[A-Za-zA-Яа-яЁё0-9]{1,20}$/i;
-const hashtagsArray = form.querySelector('.text__hashtags').value.split(' ');
 
-function validateHashtags (value) {
-  re.test(value);
+function validateHashtag (value) {
   return re.test(value);
 }
 
-function validateHashtagsPattern (value) {
-  let checkPattern;
-  for (let i = 0; i < value.length; i++) {
-    checkPattern = validateHashtags(value[i]);
-    if (checkPattern === false) {
-      break;
+function validateHashtagsPattern () {
+  const arrayInput = userFormElement.querySelector('.text__hashtags');
+  const hashtagsArray = arrayInput.value.split(' ');
+
+  if (arrayInput.value.length) {
+    let checkPattern;
+    for (let i = 0; i < hashtagsArray.length; i++) {
+      checkPattern = validateHashtag(hashtagsArray[i]);
+      if (checkPattern === false) {
+        break;
+      }
     }
+    return checkPattern;
   }
-  // console.log(checkPattern);
-  return checkPattern;
+  return true;
 }
 
-validateHashtagsPattern(hashtagsArray);
+function validateHashtagsAmount () {
+  const arrayInput = userFormElement.querySelector('.text__hashtags');
+  const hashtagsArray = arrayInput.value.split(' ');
+  return hashtagsArray.length <= 5;
+}
 
-// Функция вроде работает, с предустановленным value в text__hashtags, но когда доходит до валидации ввода сразу ошибка
+function validateHashtagsUnique () {
+  const arrayInput = userFormElement.querySelector('.text__hashtags');
+  const hashtagsArray = arrayInput.value.toLowerCase().split(' ');
+  const hashtagsUniqueArray = Array.from(new Set(hashtagsArray));
+  return hashtagsArray.length === hashtagsUniqueArray.length;
+}
 
-pristine.addValidator(form.querySelector('.text__hashtags'), validateHashtagsPattern, 'Неверно');
+pristine.addValidator(userFormElement.querySelector('.text__hashtags'), validateHashtagsPattern, 'Неверный ввод');
+pristine.addValidator(userFormElement.querySelector('.text__hashtags'), validateHashtagsAmount, 'Не больше 5 хэштегов');
+pristine.addValidator(userFormElement.querySelector('.text__hashtags'), validateHashtagsUnique, 'Используйте уникальные хэштеги');
 
-form.addEventListener('submit', (evt) => {
+userFormElement.addEventListener('submit', (evt) => {
   evt.preventDefault();
-
-  const isValid = pristine.validate();
-  if (isValid) {
-    // console.log('Можно отправлять');
-  } else {
-    // console.log('Форма невалидна');
-  }
+  pristine.validate();
 });
-
-// form.addEventListener('submit', (evt) => {
-//   evt.preventDefault();
-//   pristine.validate();
-// });
--- js/user-form.js
+++ js/user-form.js
@@ -1,77 +1,20 @@
+const userFormElement = document.querySelector('.img-upload__form');
 const uploadImgElement = document.querySelector('#upload-file');
 const textHashtagsElement = document.querySelector('.text__hashtags');
-const textDescriptionElement = document.querySelector('.text__description');
+const userCommentElement = document.querySelector('.text__description');
 const uploadImgCloseElement = document.querySelector('.img-upload__cancel');
 const imgOverlayElement = document.querySelector('.img-upload__overlay');
 const bodyElement = document.querySelector('body');
 
-const defaultUploadImgElement = uploadImgElement.value;
-const defaultTextHashtagsElement = textHashtagsElement.value;
-const defaultTextDescriptionElement = textDescriptionElement.value;
-const effectsRadioElements = document.querySelectorAll('.effects__radio');
-const defaultRadioElement = document.querySelector('#effect-none');
-
-const resetUserModal = () => {
-  effectsRadioElements.forEach((element) => {
-    if (element.hasAttribute('checked') && element.id !== 'effect-none') {
-      element.removeAttribute('checked');
-    }
-  });
-
-  if (!defaultRadioElement.hasAttribute('checked')) {
-    defaultRadioElement.setAttribute('checked', '');
-  }
-
-  uploadImgElement.value = defaultUploadImgElement;
-  textHashtagsElement.value = defaultTextHashtagsElement;
-  textDescriptionElement.value = defaultTextDescriptionElement;
-};
-
 const onPopupEscKeydown = (evt) => {
-  if (evt.key === 'Escape') {
+  const isUserTyping = evt.target === textHashtagsElement || evt.target === userCommentElement;
+
+  if (evt.key === 'Escape' && !isUserTyping) {
     evt.preventDefault();
     closeUserModal ();
+    userFormElement.reset();
   }
 };
-
-// Пробовал отключить esc на фокусе этими способами
-
-// const onPopupEscKeydown = (evt) => {
-//   if (evt.key === 'Escape') {
-//     if (textHashtagsElement.onfocus) {
-//       evt.stopPropagation();
-//       console.log('Проверка2');
-//     } else {
-//       evt.preventDefault();
-//       closeUserModal ();
-//       console.log('Проверка');
-//     }
-//   }
-// };
-
-// const onPopupEscKeydown = (evt) => {
-//   if (evt.key === 'Escape' && !textHashtagsElement.onfocus) {
-//     evt.preventDefault();
-//     closeUserModal ();
-//     console.log('Проверка');
-//   }
-// };
-
-// const onPopupEscKeydown = (evt) => {
-//   if (textHashtagsElement.onfocus) {
-//     evt.stopPropagation();
-//     console.log('Проверка');
-//   } else if (evt.key === 'Escape') {
-//     evt.preventDefault();
-//     closeUserModal ();
-//   }
-// };
-
-
-// textHashtagsElement.onfocus = () => {
-//   evt.stopPropagation();
-//   console.log('Проверка');
-// };
 
 function openUserModal () {
   imgOverlayElement.classList.remove('hidden');
@@ -83,15 +26,14 @@ function openUserModal () {
 function closeUserModal () {
   imgOverlayElement.classList.add('hidden');
   bodyElement.classList.remove('modal-open');
-  resetUserModal();
 
   document.removeEventListener('keydown', onPopupEscKeydown);
 }
 
-uploadImgElement.addEventListener('input', () => {
-  openUserModal();
-});
+uploadImgElement.addEventListener('input', openUserModal);
 
 uploadImgCloseElement.addEventListener('click', () => {
   closeUserModal();
 });
+
+export {userFormElement};

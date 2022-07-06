7d455e2f15785b78ee08da05066ed876a1eed33b
Keks <keks@htmlacademy.ru>
2022-07-06 19:32:23 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form-validation.js
@@ -0,0 +1,55 @@
+import {userFormElement} from './user-form.js';
+
+const pristine = new Pristine(userFormElement, {
+  classTo: 'text__label',
+  errorTextParent: 'text__label',
+});
+
+const re = /^#[A-Za-zA-Яа-яЁё0-9]{1,20}$/i;
+
+function validateHashtag (value) {
+  return re.test(value);
+}
+
+function validateHashtagsPattern () {
+  const arrayInput = userFormElement.querySelector('.text__hashtags');
+  const hashtagsArray = arrayInput.value.split(' ');
+
+  if (arrayInput.value.length) {
+    let checkPattern;
+    for (let i = 0; i < hashtagsArray.length; i++) {
+      checkPattern = validateHashtag(hashtagsArray[i]);
+      if (checkPattern === false) {
+        break;
+      }
+    }
+    return checkPattern;
+  }
+  return true;
+}
+
+function validateHashtagsAmount () {
+  const arrayInput = userFormElement.querySelector('.text__hashtags');
+  const hashtagsArray = arrayInput.value.split(' ');
+  return hashtagsArray.length <= 5;
+}
+
+function validateHashtagsUnique () {
+  const arrayInput = userFormElement.querySelector('.text__hashtags');
+  const hashtagsArray = arrayInput.value.toLowerCase().split(' ');
+  const hashtagsUniqueArray = Array.from(new Set(hashtagsArray));
+  return hashtagsArray.length === hashtagsUniqueArray.length;
+}
+
+pristine.addValidator(userFormElement.querySelector('.text__hashtags'), validateHashtagsPattern, 'Неверный ввод');
+pristine.addValidator(userFormElement.querySelector('.text__hashtags'), validateHashtagsAmount, 'Не больше 5 хэштегов');
+pristine.addValidator(userFormElement.querySelector('.text__hashtags'), validateHashtagsUnique, 'Используйте уникальные хэштеги');
+
+// userFormElement.addEventListener('submit', (evt) => {
+//   evt.preventDefault();
+//   pristine.validate();
+// });
+
+userFormElement.addEventListener('submit', () => {
+  pristine.validate();
+});
---
+++ 7/js/main.js
@@ -0,0 +1,5 @@
+import './photo-rendering.js';
+import './user-form.js';
+import './form-validation.js';
+import './image-effects.js';
+import './image-scaling.js';
